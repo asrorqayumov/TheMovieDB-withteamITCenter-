62b82d962b23284bd15edab627057b1527ec18bd
Asror Qayumov <qayumovasror2003@gmail.com>
2023-08-11 18:03:28 +0500

    :white_check_mark: Import
    import and export initialized

---
+++ js/config.js
@@ -0,0 +1,11 @@
+
+
+export  let config = {
+    "BASE_URL":'https://api.themoviedb.org/3/',
+    "TOKEN" :" eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjM2ZDI2Yjc0MDIxMzQ1MjgxZDVkNGY2YWFlMmNiMiIsInN1YiI6IjY0ZDUzMDAyYjZjMjY0MTE1NGY3ZGM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KsAIE9wp69o-UDwt4eKPGq9GNBKmfcSjBD_iORozxJw",
+    "BASE_IMG_URL":'https://image.tmdb.org/t/p/original/'
+}
+
+export  function getMovies() {
+    console.log('bu exportdan kelgan function');
+}
