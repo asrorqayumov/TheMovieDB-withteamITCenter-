f77e5d49461780c56318e3cffa053088a6d40bff
Asror Qayumov <qayumovasror2003@gmail.com>
2023-08-25 17:24:45 +0500

    :zap: Popular movie page improved

--- js/apis/popularApi.js
+++ js/apis/popularApi.js
@@ -20,3 +20,25 @@ export async function getPopularMoviesWeek() {
     console.log(error);
   }
 }
+
+
+export async function filterMovies(value) {
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjM2ZDI2Yjc0MDIxMzQ1MjgxZDVkNGY2YWFlMmNiMiIsInN1YiI6IjY0ZDUzMDAyYjZjMjY0MTE1NGY3ZGM5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KsAIE9wp69o-UDwt4eKPGq9GNBKmfcSjBD_iORozxJw'
+      }
+    };
+    let request = await fetch(
+      `${Config.BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=${value}`,
+      options
+    );
+    let response = await request.json();
+    console.log(response);
+    // return response.results;
+  } catch (error) {
+    console.log(error);
+  }
+}
