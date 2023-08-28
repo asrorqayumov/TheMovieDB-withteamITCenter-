0877500d625ca30c3da7de6f58ce88dbe1f4e0a8
Asror Qayumov <qayumovasror2003@gmail.com>
2023-08-28 17:42:28 +0500

    :rocket: Promise all initalized

--- js/router.js
+++ js/router.js
@@ -1,8 +1,8 @@
 import {
-    displayTodayTrendingMovies,
-    displayTodayTrendingMoviesweek,
-    displayTvSeriesListMoviesTv,
-    displayTvSeriesListMoviesTheater
+  displayTodayTrendingMovies,
+  displayTodayTrendingMoviesweek,
+  displayTvSeriesListMoviesTv,
+  displayTvSeriesListMoviesTheater,
 } from "./pages/home.js";
 import { displayPopular } from "./pages/popular-movie.js";
 import { FilteredMovies } from "./pages/popular-movie.js";
@@ -13,82 +13,52 @@ import { getTrendingMoviesWeek } from "./apis/homeApi.js";
 import { getTvSeriesListMoviesTv } from "./apis/homeApi.js";
 import { getTvSeriesListMoviesTheater } from "./apis/homeApi.js";
 
+window.addEventListener('popstate', (e)=>{
+    location.reload()
+})
+
 document.addEventListener("DOMContentLoaded", async (e) => {
-    if (location.pathname == "index.html" || location.pathname == "/") {
-        let getMovieRequestToday = await getTrendingMoviesToday();
-        displayTodayTrendingMovies(getMovieRequestToday).then(() => {
-            let cards = document.querySelectorAll('.card__img__poster');
-            cards.forEach((card) => {
-                card.addEventListener('click', (e) => {
-                    let id = e.target.id;
-                    history.pushState({ id }, null, 'movie.html');
-                    location.reload()
-                })
-            })
-        })
-
-        let getMovieRequestWeek = await getTrendingMoviesWeek();
-        displayTodayTrendingMoviesweek(getMovieRequestWeek).then(() => {
-            let cards = document.querySelectorAll('.card__img__poster');
-            cards.forEach((card) => {
-                card.addEventListener('click', (e) => {
-                    let id = e.target.id;
-                    history.pushState({ id }, null, 'movie.html');
-                    location.reload()
-                })
-            })
-        })
-
-        let getMovieRequestOnTv = await getTvSeriesListMoviesTv();
-        displayTvSeriesListMoviesTv(getMovieRequestOnTv).then(() => {
-            let cards = document.querySelectorAll('.card__img__poster');
-            cards.forEach((card) => {
-                card.addEventListener('click', (e) => {
-                    let id = e.target.id;
-                    history.pushState({ id }, null, 'movie.html');
-                    location.reload()
-                })
-            })
-        })
-
-        let getMovieRequestTheater = await getTvSeriesListMoviesTheater();
-        displayTvSeriesListMoviesTheater(getMovieRequestTheater).then(() => {
-            let cards = document.querySelectorAll('.card__img__poster');
-            cards.forEach((card) => {
-                card.addEventListener('click', (e) => {
-                    let id = e.target.id;
-                    history.pushState({ id }, null, 'movie.html');
-                    location.reload()
-                })
-            })
-        })
-    }
-    
-
-
-    else if (location.pathname == "/popularMovie.html" || location.pathname == "popularMovie.html") {
-        let formHandler = document.querySelector('.form__search');
-        FilteredMovies(formHandler)
-        getPopularMoviesWeek()
-            .then((data) => {
-                displayPopular(data);
-            })
-            .then((data) => {
-                let cards = document.querySelectorAll('.card__img__poster');
-                cards.forEach((card) => {
-                    card.addEventListener('click', (e) => {
-                        let id = e.target.id;
-                        history.pushState({ id }, null, 'movie.html');
-                        location.reload()
-                    })
-                })
-            })
-    }
-
-
-    else if (location.pathname == "/movie.html") {
-        // ...
-    }
+  if (location.pathname == "index.html" || location.pathname == "/") {
+    Promise.all([
+      getTrendingMoviesToday(),
+      getTrendingMoviesWeek(),
+      getTvSeriesListMoviesTv(),
+      getTvSeriesListMoviesTheater(),
+    ]).then((data) => {
+      displayTodayTrendingMovies(data[0]);
+      displayTodayTrendingMoviesweek(data[1]);
+      displayTvSeriesListMoviesTv(data[2]);
+      displayTvSeriesListMoviesTheater(data[3]);
+      let cards = document.querySelectorAll(".card__img__poster");
+      cards.forEach((card) => {
+        card.addEventListener("click", (e) => {
+          let id = e.target.id;
+          history.pushState({ id }, null, "movie.html");
+          location.reload();
+        });
+      });
+    });
+  } else if (
+    location.pathname == "/popularMovie.html" ||
+    location.pathname == "popularMovie.html"
+  ) {
+    let formHandler = document.querySelector(".form__search");
+    FilteredMovies(formHandler);
+    getPopularMoviesWeek()
+      .then((data) => {
+        displayPopular(data);
+      })
+      .then((data) => {
+        let cards = document.querySelectorAll(".card__img__poster");
+        cards.forEach((card) => {
+          card.addEventListener("click", (e) => {
+            let id = e.target.id;
+            history.pushState({ id }, null, "movie.html");
+            location.reload();
+          });
+        });
+      });
+  } else if (location.pathname == "/movie.html") {
+    // ...
+  }
 });
-
-
