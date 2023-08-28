d46dfbcbe4218dac7be0698163319e57684b560b
Asror Qayumov <qayumovasror2003@gmail.com>
2023-08-28 13:37:17 +0500

    'router'

--- js/router.js
+++ js/router.js
@@ -7,17 +7,16 @@ import { getTrendingMoviesToday } from "./apis/homeApi.js";
 document.addEventListener("DOMContentLoaded", async (e) => {
   if (location.pathname == "index.html" || location.pathname == "/") {
     let getMovieRequest = await getTrendingMoviesToday();
-    displayTodayTrendingMovies(getMovieRequest).then(()=>{
-        let cards = document.querySelectorAll('.card__img__poster');
-        cards.forEach((card)=>{
-            card.addEventListener('click', (e)=>{
-                let id = e.target.id;
-                history.pushState({id}, null, 'movie.html');
-                location.reload()
-            })
-        })
-    })
- 
+    displayTodayTrendingMovies(getMovieRequest).then(() => {
+      let cards = document.querySelectorAll(".card__img__poster");
+      cards.forEach((card) => {
+        card.addEventListener("click", (e) => {
+          let id = e.target.id;
+          history.pushState({ id }, null, "movie.html");
+          location.reload();
+        });
+      });
+    });
   } else if (location.pathname == "popularMovie.html") {
     // ...
   } else if (location.pathname == "/movie.html") {
